a19ec3ed45fa50d982b538a425d66bb8116bdad8
Andrew Aikman <a.aikman@imperial.ac.uk>
2018-03-16 16:14:17 +0000

    Added python setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='csv-to-json',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='MIT 3.0',
+    author='aiky30',
+    author_email='aiky30@yahoo.com',
+    description='Convert a flat CSV file into a JSON object'
+)
